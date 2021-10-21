c543e0ae66a13ec8e754bfd8346cca5afc402159
TheMinds3t <42127046+TheMinds3t@users.noreply.github.com>
2021-10-21 14:30:25 -0500

    Added GuiButton class
    Added abstract GuiButton class
    Added example implementation in Main

--- src/cs410/lanbros/main/Main.java
+++ src/cs410/lanbros/main/Main.java
@@ -1,9 +1,30 @@
 package cs410.lanbros.main;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import cs410.lanbros.gui.GuiButton;
+
 public class Main 
 {
 	public static void main(String[] args)
 	{
+		JFrame frame = new JFrame("Demo");
+		frame.setBounds(100, 100, 900, 600);
+		frame.setLocationRelativeTo(null); //center GUI
+		JPanel panel = new JPanel();
+		frame.add(panel);
 		
+		GuiButton button = new GuiButton("Testing!") {
+			private static final long serialVersionUID = 0;
+			
+			public void onClick(boolean pressed)
+			{
+				System.out.println("Button pressed? " + pressed);
+			}
+		};
+		
+		panel.add(button);
+		frame.setVisible(true);
 	}
 }
