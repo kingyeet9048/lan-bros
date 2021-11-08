2b89281abc80d0799ad365fca2625fb863cd8c1c
Sully <70219727+kingyeet9048@users.noreply.github.com>
2021-11-08 10:23:41 -0600

    Backend/rework packet classes (#13)
    * changes
    
    * chnges
    
    * fixing merge conflict
    
    * testing peered connection
    
    * new gitignores
    
    * further work
    
    * small change to server and serverworker
    
    * more progress. with test
    
    * current
    
    * added jar
    
    * added movement support
    
    * gson implementation
    
    * small changes
    
    * fixed the connection termination  bug
    
    * moved to concurrenthashmap to deal with the no collection lock when two thread are modifying the same hashmap
    
    * some client logic
    
    * merge conflict resolution
    
    * small change
    
    * improved routing logic
    
    * console outs
    
    * commentted the entire network handler code'
    
    * method to tell server that this player is moving a specific direction
    
    * fixing part 1
    
    Co-authored-by: Bada <rp5626vi@winona.edu>
    Co-authored-by: nameer1811 <nameer.fahad@gmail.com>

--- src/cs410/lanbros/networkhandler/Server/Listener.java
+++ src/cs410/lanbros/networkhandler/Server/Listener.java
@@ -1,15 +1,27 @@
-package cs410.lanbros.networkhandler.server;
+package cs410.lanbros.networkhandler.Server;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * Server Listener for new connections
+ * 
+ * @author Sulaiman Bada
+ */
 public class Listener implements Runnable {
 
+    // instance variables
     private ServerSocket listen;
     private Server server;
 
+    /**
+     * Constuctor needs server socket and server instance.
+     * 
+     * @param listen
+     * @param server
+     */
     public Listener(ServerSocket listen, Server server) {
         this.listen = listen;
         this.server = server;
@@ -17,12 +29,15 @@ public class Listener implements Runnable {
 
     @Override
     public void run() {
-        // TODO Auto-generated method stub
+        // while server is not closed
         while (!listen.isClosed()) {
             try {
+                // accepts a new connection
                 Socket newConnection = listen.accept();
                 System.out.println("Received new Connection from: " + newConnection.getInetAddress().getHostName());
+                // if the connection size is greater then the server limit
                 if (server.getWorkers().size() >= server.getMAX_PLAYERS()) {
+                    // tell the connection that the lobby is full
                     OutputStream stream = newConnection.getOutputStream();
                     if (newConnection.isConnected() && newConnection.isOutputShutdown()) {
                         stream.write("Full Lobby\n".getBytes());
@@ -30,6 +45,9 @@ public class Listener implements Runnable {
                     }
                     // newConnection.close();
                 } else {
+                    // lobby not full, process the connection by giving it its own worker and start
+                    // its thread.
+                    // this worker will listen for any requests from it.
                     ServerWorker newWorker = new ServerWorker(newConnection, server);
                     Thread newServerThread = new Thread(newWorker);
                     newServerThread.start();
