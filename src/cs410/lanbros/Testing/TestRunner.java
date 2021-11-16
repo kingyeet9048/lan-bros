a21e0d272438f45e2076dcfe31c83d18cc3e2a31
kingyeet9048 <sulaimanbada8048@gmail.com>
2021-11-16 09:22:06 -0600

    developing junit tests

---
+++ src/cs410/lanbros/Testing/TestRunner.java
@@ -0,0 +1,5 @@
+package cs410.lanbros.Testing;
+
+public class TestRunner {
+
+}
---
+++ src/cs410/lanbros/Testing/UseCaseTesting.java
@@ -0,0 +1,8 @@
+package cs410.lanbros.Testing;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class UseCaseTesting {
+
+}
